32593ba20eee7937a1f916e5a8b597c02fea37c2
siti madihah <ctmadihahghozali@gmail.com>
2020-07-15 19:34:00 +0800

    cthijab

---
+++ lib/register.dart
@@ -0,0 +1,271 @@
+import 'package:flutter/material.dart';
+import 'package:lab2/login.dart';
+import 'package:http/http.dart' as http;
+import 'package:toast/toast.dart';
+
+void main() => runApp(RegisterScreen());
+
+class RegisterScreen extends StatefulWidget {
+  @override
+  _RegisterScreenState createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  double screenHeight;
+  bool _isChecked = false;
+  String urlRegister =
+      "http://saujanaeclipse.com/cthijab/php/register_user.php";
+  TextEditingController nameEditingController = new TextEditingController();
+  TextEditingController emailEditingController = new TextEditingController();
+  TextEditingController phoneditingController = new TextEditingController();
+  TextEditingController passEditingController = new TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    screenHeight = MediaQuery.of(context).size.height;
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: new ThemeData(
+        primarySwatch: Colors.brown,
+      ),
+      title: 'Material App',
+      home: Scaffold(
+          resizeToAvoidBottomPadding: false,
+          body: Stack(
+            children: <Widget>[
+              Container(
+                  margin: EdgeInsets.only(bottom: 250),
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage('assets/images/logo.png'),
+                          fit: BoxFit.fill))),
+              lowerHalf(context),
+              pageTitle(),
+            ],
+          )),
+    );
+  }
+
+  Widget lowerHalf(BuildContext context) {
+    return Container(
+      height: 400,
+      margin: EdgeInsets.only(top: screenHeight / 2.5),
+      padding: EdgeInsets.only(left: 10, right: 10),
+      child: Column(
+        children: <Widget>[
+          Card(
+            elevation: 10,
+            child: Container(
+              padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+              child: Column(
+                children: <Widget>[
+                  Align(
+                    alignment: Alignment.topCenter,
+                    child: Text(
+                      "REGISTER",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 26,
+                        fontWeight: FontWeight.w800,
+                      ),
+                    ),
+                  ),
+                  TextField(
+                      controller: nameEditingController,
+                      keyboardType: TextInputType.text,
+                      decoration: InputDecoration(
+                        labelText: 'Name',
+                        icon: Icon(Icons.person),
+                      )),
+                  TextField(
+                      controller: emailEditingController,
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                        labelText: 'Email',
+                        icon: Icon(Icons.email),
+                      )),
+                  TextField(
+                      controller: phoneditingController,
+                      keyboardType: TextInputType.phone,
+                      decoration: InputDecoration(
+                        labelText: 'Phone',
+                        icon: Icon(Icons.phone),
+                      )),
+                  TextField(
+                    controller: passEditingController,
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      icon: Icon(Icons.lock),
+                    ),
+                    obscureText: true,
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Checkbox(
+                        value: _isChecked,
+                        onChanged: (bool value) {
+                          _onChange(value);
+                        },
+                      ),
+                      GestureDetector(
+                        onTap: _showEULA,
+                        child: Text('I Agree to Terms  ',
+                            style: TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.bold)),
+                      ),
+                      MaterialButton(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(5.0)),
+                        minWidth: 115,
+                        height: 50,
+                        child: Text('Register'),
+                        color: Colors.blueAccent,
+                        textColor: Colors.white,
+                        elevation: 5,
+                        onPressed: _onRegister,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text("Already register? ", style: TextStyle(fontSize: 16.0)),
+              GestureDetector(
+                onTap: _loginScreen,
+                child: Text(
+                  "Login",
+                  style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget pageTitle() {
+    return Container(
+      //color: Color.fromRGBO(255, 200, 200, 200),
+      margin: EdgeInsets.only(top: 70),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            " your SAFETY,our PRIORITY ",
+            style: TextStyle(
+                fontSize: 20,
+                color: Colors.blueAccent,
+                fontWeight: FontWeight.w800,
+                fontStyle: FontStyle.italic),
+          )
+        ],
+      ),
+    );
+  }
+
+  void _onRegister() {
+    String name = nameEditingController.text;
+    String email = emailEditingController.text;
+    String phone = phoneditingController.text;
+    String password = passEditingController.text;
+    if (!_isChecked) {
+      Toast.show("Accept Term", context,
+          duration: Toast.LENGTH_LONG, gravity: Toast.BOTTOM);
+      return;
+    }
+
+    http.post(urlRegister, body: {
+      "name": name,
+      "email": email,
+      "password": password,
+      "phone": phone,
+    }).then((res) {
+      if (res.body == "success") {
+        Navigator.pop(
+            context,
+            MaterialPageRoute(
+                builder: (BuildContext context) => LoginScreen()));
+        Toast.show("Registration success", context,
+            duration: Toast.LENGTH_LONG, gravity: Toast.BOTTOM);
+      } else {
+        Toast.show("Registration failed", context,
+            duration: Toast.LENGTH_LONG, gravity: Toast.BOTTOM);
+      }
+    }).catchError((err) {
+      print(err);
+    });
+  }
+
+  void _loginScreen() {
+    Navigator.pop(context,
+        MaterialPageRoute(builder: (BuildContext context) => LoginScreen()));
+  }
+
+  void _onChange(bool value) {
+    setState(() {
+      _isChecked = value;
+      //savepref(value);
+    });
+  }
+
+  void _showEULA() {
+    // flutter defined function
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        // return object of type Dialog
+        return AlertDialog(
+          title: new Text("EULA"),
+          content: new Container(
+            height: screenHeight / 2,
+            child: Column(
+              children: <Widget>[
+                Expanded(
+                  flex: 1,
+                  child: new SingleChildScrollView(
+                    child: RichText(
+                        softWrap: true,
+                        textAlign: TextAlign.justify,
+                        text: TextSpan(
+                            style: TextStyle(
+                              color: Colors.black87,
+
+                              //fontWeight: FontWeight.w500,
+                              fontSize: 15.0,
+                            ),
+                            text:
+                                "This End-User License Agreement is a legal agreement between you and Saujanae This EULA agreement governs your acquisition and use of our MY.BUS software (Software) directly from Saujanae or indirectly through a Sauajane authorized reseller or distributor (a Reseller).Please read this EULA agreement carefully before completing the installation process and using the MY.BUS software. It provides a license to use the MY.BUS software and contains warranty information and liability disclaimers. If you register for a free trial of the MY.BUS software, this EULA agreement will also govern that trial. By clicking accept or installing and/or using the MY.BUS software, you are confirming your acceptance of the Software and agreeing to become bound by the terms of this EULA agreement. If you are entering into this EULA agreement on behalf of a company or other legal entity, you represent that you have the authority to bind such entity and its affiliates to these terms and conditions. If you do not have such authority or if you do not agree with the terms and conditions of this EULA agreement, do not install or use the Software, and you must not accept this EULA agreement.This EULA agreement shall apply only to the Software supplied by Saujanae herewith regardless of whether other software is referred to or described herein. The terms also apply to any Slumberjer updates, supplements, Internet-based services, and support services for the Software, unless other terms accompany those items on delivery. If so, those terms apply. This EULA was created by EULA Template for MY.GROCERY. Slumberjer shall at all times retain ownership of the Software as originally downloaded by you and all subsequent downloads of the Software by you. The Software (and the copyright, and other intellectual property rights of whatever nature in the Software, including any modifications made thereto) are and shall remain the property of Slumberjer. Slumberjer reserves the right to grant licences to use the Software to third parties"
+                            //children: getSpan(),
+                            )),
+                  ),
+                )
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            // usually buttons at the bottom of the dialog
+            new FlatButton(
+              child: new Text("Close"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            )
+          ],
+        );
+      },
+    );
+  }
+}
---
+++ lib/splashscreen.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:lab2/login.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      home: Scaffold(
+        backgroundColor: Colors.red[100],
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Image.asset(
+                'assets/images/logo.png',
+                width: 230,
+                height: 230,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              new ProgressIndicator(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ProgressIndicator extends StatefulWidget {
+  @override
+  _ProgressIndicatorState createState() => new _ProgressIndicatorState();
+}
+
+class _ProgressIndicatorState extends State<ProgressIndicator>
+    with SingleTickerProviderStateMixin {
+  AnimationController controller;
+  Animation<double> animation;
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+        duration: const Duration(milliseconds: 2000), vsync: this);
+    animation = Tween(begin: 0.0, end: 1.0).animate(controller)
+      ..addListener(() {
+        setState(() {
+          //updating states
+          if (animation.value > 0.99) {
+            Navigator.pushReplacement(context,
+                MaterialPageRoute(builder: (BuildContext context) => LoginScreen()));
+          }
+        });
+      });
+    controller.repeat();
+  }
+
+  @override
+  void dispose() {
+    controller.stop();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Center(
+        child: new Container(
+     
+      color: Colors.redAccent,
+      child:  CircularProgressIndicator (
+       
+        backgroundColor: Colors.black,
+       
+      ),
+    ));
+  }
+}
+
